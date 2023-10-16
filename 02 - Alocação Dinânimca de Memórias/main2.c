3414e1953e02a9b2a6099725c36e476af3148447
Joao Caetano <jvc.caetanim@hotmail.com>
2023-10-16 09:43:34 -0300

    Adicionando outra atividade

--- 02 - Alocação Dinânimca de Memórias/main2.c
+++ 02 - Alocação Dinânimca de Memórias/main2.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define TAM 255
 // Definindo a estrutura Pessoa
-struct Pessoa {
+struct Pessoa
+{
     char CPF[12];
     char nome[50];
     float salario;
 };
 
+
 // Função para alocar memória para um vetor de estruturas Pessoa
-struct Pessoa *alocarMemoria(int tamanho) {
+struct Pessoa *alocarMemoria(int tamanho)
+{
     struct Pessoa *vetor = (struct Pessoa *)calloc(tamanho, sizeof(struct Pessoa));
-    if (vetor == NULL) {
+    if (vetor == NULL)
+    {
         printf("Falha na alocação de memória.\n");
         exit(EXIT_FAILURE);
     }
@@ -19,25 +23,29 @@ struct Pessoa *alocarMemoria(int tamanho) {
 }
 
 // Função para preencher um vetor de estruturas Pessoa
-void preencherVetor(struct Pessoa *vetor, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
+void preencherVetor(struct Pessoa *vetor, int tamanho)
+{
+    char lixo;
+    for (int i = 0; i < tamanho; i++)
+    {
         printf("Digite o CPF da Pessoa %d: \n", i + 1);
         scanf("%s", vetor[i].CPF);
-        
+
         printf("Digite o nome da Pessoa %d: \n", i + 1);
         scanf("%s[^\n]", vetor[i].nome);
-        scanf("%c");
-        
+        // scanf("%c");
+        //fgets(vetor[i].nome, 50, stdin);
         printf("Digite o salário da Pessoa %d: \n", i + 1);
         scanf("%f", &vetor[i].salario);
-        
     }
 }
 
 // Função para imprimir os elementos de um vetor de estruturas Pessoa
-void imprimirVetor(struct Pessoa *vetor, int tamanho) {
+void imprimirVetor(struct Pessoa *vetor, int tamanho)
+{
     printf("Vetor de Pessoas:\n");
-    for (int i = 0; i < tamanho; i++) {
+    for (int i = 0; i < tamanho; i++)
+    {
         printf("Pessoa %d:\n", i + 1);
         printf("CPF: %s\n", vetor[i].CPF);
         printf("Nome: %s\n", vetor[i].nome);
@@ -46,7 +54,8 @@ void imprimirVetor(struct Pessoa *vetor, int tamanho) {
     }
 }
 
-int main() {
+int main()
+{
     struct Pessoa *array;
     int n;
 
